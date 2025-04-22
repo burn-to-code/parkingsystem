09de2f6f3ade203c33cb53962d877461df1af5cb
Dylan Senasson <dylan.senasson@outlook.fr>
2025-04-22 16:51:10 +0200

    Ajout de la méthode getNbTickets, dans les méthodes processIncomingVehicle et ProcessExitingVehicle pour savoir quand effectuer l'apparition du message et faire la ristourne de 5% à l'utilisateur.

--- src/main/java/com/parkit/parkingsystem/service/ParkingService.java
+++ src/main/java/com/parkit/parkingsystem/service/ParkingService.java
@@ -32,6 +32,9 @@ public class ParkingService {
             ParkingSpot parkingSpot = getNextParkingNumberIfAvailable();
             if(parkingSpot !=null && parkingSpot.getId() > 0){
                 String vehicleRegNumber = getVehichleRegNumber();
+                if (ticketDAO.getNbTickets(vehicleRegNumber)) {
+                    System.out.println("Heureux de vous revoir ! En tant qu’utilisateur régulier de notre parking, vous allez obtenir une remise de 5%");
+                }
                 parkingSpot.setAvailable(false);
                 parkingSpotDAO.updateParking(parkingSpot);//allot this parking space and mark it's availability as false
 
@@ -103,7 +106,10 @@ public class ParkingService {
             Ticket ticket = ticketDAO.getTicket(vehicleRegNumber);
             Date outTime = new Date();
             ticket.setOutTime(outTime);
-            fareCalculatorService.calculateFare(ticket);
+            if (ticketDAO.getNbTickets(vehicleRegNumber)) {
+                fareCalculatorService.calculateFare(ticket, true);
+            } else
+                fareCalculatorService.calculateFare(ticket);
             if(ticketDAO.updateTicket(ticket)) {
                 ParkingSpot parkingSpot = ticket.getParkingSpot();
                 parkingSpot.setAvailable(true);
